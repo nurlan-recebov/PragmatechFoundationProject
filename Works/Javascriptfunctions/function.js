5934fddd4ad1570c6c464ad7973282185c6350bf
Nurlan-recebov <71444150+nurlan-recebov@users.noreply.github.com>
2022-01-01 18:49:51 +0400

    tapsiriq.html

---
+++ Works/Javascriptfunctions/function.js
@@ -0,0 +1,121 @@
+function numbertostring(x=string ,_default=String){
+    console.log(x);
+    return String(x);
+}
+numbertostring(678)
+function numberincrease(x=number , _default=Number){
+    console.log(x=x+1);
+    return(x=x+1);
+}
+numberincrease(6)
+function numberdecrease( x=number, _default=Number){
+    console.log(x=x-2);
+    return(x=x-2);
+}
+numberdecrease(12)
+function add(x,y){
+    console.log(x+y);
+    return(x+y)
+}
+add(12,13)
+function subtract(x,y){
+    console.log(x-y);
+    return(x-y)
+
+}
+subtract(40,20)
+function multiplay(x,y){
+    console.log(x*y);
+    return(x*y);
+}
+multiplay(40,3)
+function divide(x,y) {
+    console.log(x/y);
+    return(x/y)
+}
+divide(100,2)
+let square= Math.pow(2,4);
+console.log(square);
+
+function calculate(operation, x, y) {
+    operation = 3
+    x = 4
+    
+    console.log(y = operation + x)
+    return y;
+}
+calculate(3, 4, 7)
+function isGreaterThan(a,b){
+    if(a>b){
+        return console.log(true);
+    }
+    else console.log(false);
+    
+}
+isGreaterThan(20,10)
+function areequal(a,b) {
+    if(a===b){
+        console.log(true);
+    }
+    else console.log(false);
+}
+areequal(20,10)
+
+function minimum(a,b) {
+     let small
+    if(a<b){
+        a==small
+        console.log(a);
+    }
+    else{
+        console.log(b);
+    }
+    
+
+    console.log(small);
+    
+    
+}
+
+minimum(12,13)
+function maxmimum(a,b) {
+    let big
+   if(a>b){
+       a==big
+       console.log(a);
+   }
+   else{
+       console.log(b);
+   }
+   
+
+   console.log(big);
+   
+   
+}
+
+maxmimum(200,13)
+function iseven(a) {
+    if(a%2==0){
+        console.log(true);
+    }
+    else console.log(false);
+}
+iseven(40)
+function isodd(a) {
+    if(a%2!==0){
+        console.log(true);
+    }
+
+    else console.log(false);
+    
+}
+isodd(24)
+function combine(word1,word2){
+    
+    console.log(word1 + " " + word2)
+    
+}
+combine('Dunya','gozeli')
+
+
---
+++ Works/tapsiriq/js/app.js
@@ -0,0 +1,6 @@
+let cumle='Memmedhesen'
+let ulduzlar=''
+for(let i=1; i<=cumle.length;i++){
+    ulduzlar+='*'
+    console.log(ulduzlar);
+}
