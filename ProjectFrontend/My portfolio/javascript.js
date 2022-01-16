6d50ecd61ce3977763a44c2bd505f9a6662bf463
Nurlan-recebov <71444150+nurlan-recebov@users.noreply.github.com>
2022-01-16 15:22:00 +0400

    portfolio.html

--- ProjectFrontend/My portfolio/javascript.js
+++ ProjectFrontend/My portfolio/javascript.js
@@ -4,16 +4,15 @@
 //     slide.style.transform="translateX(800px)";
 // }
 let button = document.querySelector("#up")
-console.log(button)
+let right = document.querySelector(".right")
+let shadow = document.querySelector(".shadow")
+
 button.addEventListener('click',()=>{
     
-    console.log("hjgh");
+
     window.scrollTo({ top: 0, behavior: 'smooth' });
     
 })
 
-document.getElementById("Home")=function(){
-    
-        document.getElementById("up").style.display="none"  
 
-}
+
